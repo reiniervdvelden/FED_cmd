45f1c5c1fafa0353c037cef5f0b5db8abeb2f86c
reiniervdvelden <90217768+reiniervdvelden@users.noreply.github.com>
2021-10-02 23:43:32 +0200

    veel updates

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,1 +1,16 @@
 // JavaScript Document
+var coin = document.querySelector('#coin'); //coin svg
+
+document.addEventListener("keypress", function(event) {
+  if (event.keyCode == 13) {
+    coin.classList.add("coin");
+  }
+});
+
+var laserstraal = document.querySelector('#laserstraal'); //laserstraal png
+
+document.addEventListener("keypress", function(event) {
+  if (event.keyCode == 48) {
+    laserstraal.classList.add("animation_laser_straal");
+  }
+});
